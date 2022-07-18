2793adb84618c3cfd9fccf7017e5652f434adefb
nazib1234 <44441569+nazib1234@users.noreply.github.com>
2022-07-17 23:09:07 -0500

    Update apps.py
    Updated Login and Signup Functionality

--- apps.py
+++ apps.py
@@ -1,6 +1,10 @@
-from bottle import route, run, template, request, response, post, get, delete, put
+from bottle import route, run, template, request
+from bottle import response, post, get, delete, put, view, redirect, response
+import uuid
 import os
-import sqlite3     
+import sqlite3
+
+sessions = {}
 
 db = sqlite3.connect('forumdata.db')
 cur = db.cursor()
@@ -9,18 +13,18 @@ abs_app_dir_path = os.path.dirname(os.path.realpath(__file__))
 abs_views_path = os.path.join(abs_app_dir_path, 'views')
 
 @route('/')
+@view('/')
 def index():
-    return template('index.tpl')
+    user_session_id = request.get_cookie("user_session_id")
+    if not user_session_id or user_session_id not in sessions:
+        return template("index.tpl", user="Guest")
+    else:
+        user = sessions[user_session_id]
+        return template("index.tpl", user=user)
 
 @route('/login')
 def login():
-    return '''
-        <form action="/login" method="post">
-            username: <input name="username" type="text" />
-            password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-           '''
+    return template('Login.tpl', LogInFailed=False, Registration_Success=False)
 
 @route('/login', method='POST') 
 def do_login():
@@ -29,36 +33,41 @@ def do_login():
     statement = f"SELECT username from users WHERE username='{username}' AND Password = '{password}';"
     cur.execute(statement)
     if not cur.fetchone():
-        return "<p>Your log in attempt has failed</p>"
-    else:
-        return "<p>You have logged in successfuly</p>"
+        return template("Login.tpl", LogInFailed=True, Registration_Success=False)
+    user_session_id = str(uuid.uuid4())
+    statement = f"SELECT Username, First_Name, Last_Name, Email_Address, Registered_Date, isAdmin FROM Users Where Username = '{username}'"
+    cur.execute(statement)
+    sessions[user_session_id] = cur.fetchall()
+    response.set_cookie("user_session_id", user_session_id)
+    return redirect('/')
 
 @route('/signup')
 def signup():
-    return '''
-        <form action="/signup" method="post">
-            username: <input name="username" type="text" />
-            first name: <input name="first name" type="text" />
-            last name: <input name="last name" type="text" />
-            email address: <input name="email address" type="text" />
-            password: <input name="password" type="password" />
-            confirm password: <input name="confirm password" type="password" />
-            <input value="Signup" type="submit" />
-        </form>
-           '''
+    return template('Signup.tpl', Email_Taken=False, Username_Taken=False, Not_Same_Password=False)
 
 @route('/signup', method='POST') 
 def do_signup():
     username = request.forms.get('username')
     password = request.forms.get('password')
-    confirm_password = request.forms.get('confirm password')
-    first_name = request.forms.get('first name')
-    last_name = request.forms.get('last_name')
-    email = request.forms.get('email address')
+    confirm_password = request.forms.get('confirm-password')
+    first_name = request.forms.get('first-name')
+    last_name = request.forms.get('last-name')
+    email = request.forms.get('email-address')
     if password != confirm_password:
-        return "<p>Password not the same</p>"
-    statement = f"INSERT INTO Users (Username, Password, First_Name, Last_Name, Email_Address) VALUES (?,?,?,?,?)';"
+        return template('Signup.tpl', Username_Taken=False, Email_Taken=False, Not_Same_Password=True)
+    print(username)
+    statement = f"SELECT Username FROM Users WHERE Username = '{username}'"
+    cur.execute(statement)
+    if cur.fetchone() is not None:
+        return template('Signup.tpl', Username_Taken=True, Email_Taken=False, Not_Same_Password=False)
+    statement = f"SELECT Email_Address FROM Users WHERE Email_Address = '{email}'"
+    cur.execute(statement)
+    if cur.fetchone() is not None:
+        return template('Signup.tpl', Email_Taken=True, Username_Taken=False, Not_Same_Password=False)
+    statement = "INSERT INTO Users (Username, Password, First_Name, Last_Name, Email_Address, Registered_Date) VALUES (?,?,?,?,?, date('now'));"
     data_tuple = (username, password, first_name, last_name, email)
     cur.execute(statement, data_tuple)
+    db.commit()
+    return template('login.tpl', Registration_Success=True, LogInFailed=False)
 
-run(host='localhost', port=8080)
+run(host='localhost', port=8080, reloader=True)
